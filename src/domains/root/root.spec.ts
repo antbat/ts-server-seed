cce5c1e54653a75e3dd56e4087d4ef35731c85de
Anton Batalin <antbat2009@gmail.com>
2020-01-04 14:19:32 +0200

    move controller to folder with test

---
+++ src/domains/root/root.spec.ts
@@ -0,0 +1,20 @@
+import { getApp } from '../../app';
+import { Application } from 'express';
+import chai = require('chai');
+import chaiHttp = require('chai-http');
+import 'mocha';
+
+let app: Application;
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('Auth server', async () => {
+    before(async () => {
+        app = (await getApp()).application;
+    });
+    it('should return response on call', async () => {
+        const res = await chai.request(app).get('/');
+        res.status.should.be.eql(200);
+    })
+});
